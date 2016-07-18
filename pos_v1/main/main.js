babafb02e1b69b961f60e99450d8247995b76a37
DH-TB <929659475@qq.com>
2016-07-18 16:15:07 +0800

    modify pos_v1_buildReceiptItems

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -37,3 +37,39 @@ function findCartItem(item, cartItems, count) {
   return cartItems;
 }
 
+function buildReceiptItems(cartItems, promotions) {
+  let receiptItems = [];
+
+  cartItems.forEach(function (cartItem) {
+    let saved = 0;
+    let subtotal = cartItem.count * cartItem.item.price;
+
+    let promotion = findPromotionType(promotions, cartItem.item.barcode);
+
+    if (promotion === 'BUY_TWO_GET_ONE_FREE') {
+      saved = parseInt(cartItem.count / 3) * cartItem.item.price;
+      subtotal -= saved;
+    }
+    receiptItems.push({cartItem: cartItem, saved: saved, subtotal: subtotal});
+  });
+
+  return receiptItems;
+}
+
+function findPromotionType(promotions, barcode) {
+  for (let i = 0; i < promotions.length; i++) {
+    let promotion = promotions[i];
+    if (isBarcodeExist(barcode, promotion.barcodes))
+
+      return promotion.type;
+  }
+}
+
+function isBarcodeExist(barcode, barcodes) {
+  for (let i = 0; i < barcodes.length; i++) {
+    if (barcode === barcodes[i])
+
+      return true;
+  }
+  return false;
+}
--- pos_v1/test/main-test.js
+++ pos_v1/test/main-test.js
@@ -74,3 +74,89 @@ describe('pos', () => {
 
 
 
+describe('buildReceiptItems', ()=> {
+  let cartItems;
+  let promotions;
+
+  beforeEach(()=> {
+    promotions = loadPromotions();
+    cartItems = [
+      {
+        item: {
+          barcode: 'ITEM000001',
+          name: '雪碧',
+          unit: '瓶',
+          price: 3.00
+        },
+        count: 5
+      },
+      {
+        item: {
+          barcode: 'ITEM000003',
+          name: '荔枝',
+          unit: '斤',
+          price: 15.00
+        },
+        count: 2
+      },
+      {
+        item: {
+          barcode: 'ITEM000005',
+          name: '方便面',
+          unit: '袋',
+          price: 4.50
+        },
+        count: 3
+      }
+    ];
+  });
+
+  it('should print receiptItems ', ()=> {
+    const expectText = [
+      {
+        cartItem:
+          {
+            item: {
+              barcode: 'ITEM000001',
+              name: '雪碧',
+              unit: '瓶',
+              price: 3.00
+            },
+            count: 5
+          },
+        saved: 3.00,
+        subtotal: 12.00
+      },
+      {
+        cartItem:
+          {
+            item: {
+              barcode: 'ITEM000003',
+              name: '荔枝',
+              unit: '斤',
+              price: 15.00
+            },
+            count: 2
+          },
+        saved: 0.00,
+        subtotal: 30.00
+      },
+      {
+        cartItem:
+          {
+            item: {
+              barcode: 'ITEM000005',
+              name: '方便面',
+              unit: '袋',
+              price: 4.50
+            },
+            count: 3
+          },
+        saved: 4.5,
+        subtotal: 9.00
+      }
+    ];
+    expect(buildReceiptItems(cartItems,promotions)).toEqual(expectText);
+  });
+});
+
