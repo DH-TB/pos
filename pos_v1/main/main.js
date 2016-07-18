0219c8ec9bc8c3a4c4592b93fc747c988cabfe6b
DH-TB <929659475@qq.com>
2016-07-18 15:56:22 +0800

    modify pos_v1

--- pos_v1/main/main.js
+++ pos_v1/main/main.js
@@ -1,3 +1,39 @@
 'use strict';
 
-//TODO: 请在该文件中实现练习要求并删除此注释
+function buildCartItems(inputs, allItems) {
+  let cartItems = [];
+
+  inputs.forEach(function (input) {
+    let str = input.split('-');
+    let barcode = str[0];
+    let count = parseInt(str[1] || 1);
+    let item = findItem(barcode, allItems);
+    cartItems = findCartItem(item, cartItems, count);
+  });
+
+  return cartItems;
+}
+
+function findItem(barcode, allItems) {
+  for (let i = 0; i < allItems.length; i++) {
+    let item = allItems[i];
+    if (item.barcode === barcode)
+
+      return item;
+  }
+}
+
+function findCartItem(item, cartItems, count) {
+  for (let i = 0; i < cartItems.length; i++) {
+    let cartItem = cartItems[i];
+    if (item === cartItem.item) {
+      cartItem.count += count;
+
+      return cartItems;
+    }
+  }
+  cartItems.push({item: item, count: count});
+
+  return cartItems;
+}
+
